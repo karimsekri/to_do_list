b17e820f65a06c5f305d9a6cc77da73db9c3094c
karim <karim.sekri@gmail.com>
2023-08-21 16:31:29 +0200

    Ajout et suppression  des taches

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 const app = document.querySelector('#app') as HTMLDivElement;
 const divAjoutTache = document.createElement("div") as HTMLDivElement;
 const inputTache = document.createElement("input") as HTMLInputElement;
+inputTache.setAttribute("id", "input");
+inputTache.setAttribute("type", "text");
+inputTache.value = "texte ici";
+
 const btnAjouterTache = document.createElement("button") as HTMLButtonElement;
 
 const divListTaches = document.createElement("div") as HTMLDivElement;
@@ -30,8 +34,37 @@ btnAjouterTache.addEventListener("click", () => {
 
 function AjouterTache(){
   const divContainerTacheSaved = document.createElement("div") as HTMLDivElement;
-  const labelTache = document.createElement("label") as HTMLLabelElement;
 
-  divListTaches.appendChild(divContainerTacheSaved);
+  const labelTache = document.createElement("label") as HTMLLabelElement;
+  labelTache.innerText = inputTache.value;
+  
+  const btnDelete = document.createElement("button") as HTMLButtonElement;
+  btnDelete.innerText = "Remove";
+  btnDelete.addEventListener("click", () => {
+    checkboxTache.remove();
+    labelTache.remove();
+    btnDelete.remove();
+  });
+
+  const checkboxTache = document.createElement("input") as HTMLInputElement;
+  checkboxTache.setAttribute("id", "checkboxTache");
+
+  checkboxTache.addEventListener("change", () => {
+    if (checkboxTache.checked) {
+      labelTache.classList.add("checkboxTache");
+     
+    }  
+    else{
+      labelTache.classList.remove("checkboxTache");
+    }
+  });
+  checkboxTache.setAttribute("type", "checkbox");
+
+  divContainerTacheSaved.appendChild(checkboxTache);
   divContainerTacheSaved.appendChild(labelTache);
+  divContainerTacheSaved.appendChild(btnDelete);
+  divListTaches.appendChild(divContainerTacheSaved);
+  
+
+  
 }
